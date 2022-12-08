40c0dfa49601c93dac4ac7d0e1097334c71287cc
kpzaolod6000 <kpzaolod6000@gmail.com>
2022-12-08 15:38:15 -0500

    eliminacion del UseEffect

--- pages/Paginate.js
+++ pages/Paginate.js
@@ -1,6 +1,6 @@
 import _ld from 'lodash'
 
-export default const paginate = (items,pageNumber,pageSize) => {
+export default function paginate (items,pageNumber,pageSize) {
     const startIndex = (pageNumber-1) * pageSize
     return _ld(items).slice(startIndex).take(pageSize).value();
 }
